09ad949c5fcfb5dfded7558bb160d667b6183653
NatanielEscudero <130791079+NatanielEscudero@users.noreply.github.com>
2023-04-22 17:29:57 -0300

    Add files via upload

---
+++ tp2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+float ConvertirDolarAPeso(float cantidad);
+
+int main(){
+	
+	float cantidad;
+	
+	cout<<"ingrese la cantidad de dolares a convertir a pesos"<<endl;
+	cin>>cantidad;
+	
+	cout<<"su cantidad de dolares es: "<<ConvertirDolarAPeso(cantidad)<<endl;
+	
+	return 0;
+}
+
+float ConvertirDolarAPeso(float cantidad){
+	
+	float cantidaddepesos;
+	
+	cantidaddepesos=cantidad*331,37;
+	
+	return cantidaddepesos;
+}
