88a49b6104679cff7a940a3a18724c60c38eb0e0
NatanielEscudero <130791079+NatanielEscudero@users.noreply.github.com>
2023-06-02 09:55:31 -0300

    Add files via upload

---
+++ tp12.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string elCaminoAJapon(string nombre);
+
+int main(){
+	
+	string caracteres;
+
+	cout<<"ingresa una cadena de caracteres"<<endl;
+	cin>>caracteres;
+	
+	
+	cout<<"la cantidad de letras en su cadena de caracteres es: "<<caracteres.size()<<endl;
+
+	return 0;
+}
